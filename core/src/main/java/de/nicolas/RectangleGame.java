abcc54e6f998315f9217b2b58a41e415f4ab2681
nicolaswiedel <nicolaswiedel@googlemail.com>
2025-12-10 16:37:13 +0100

    1. grundlegender Aufbau
    Seite190 ( 203 pdf)

---
+++ core/src/main/java/de/nicolas/RectangleGame.java
@@ -0,0 +1,17 @@
+package de.nicolas;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import de.nicolas.utils.game.BaseGame;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class RectangleGame extends BaseGame {
+
+    @Override
+    public void create() {
+        super.create();
+        
+    }
+}
